fcbf72c969f757e5948714c0b953e8c9847c3222
Adrien Turiot <didibear77@gmail.com>
2022-10-14 03:11:17 -0400

    feat: add connection and query

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,130 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::time::Duration;
+
+use reqwest::Client;
+use serde::Deserialize;
+use serde_json::json;
+use url::Url;
+use uuid::Uuid;
+
+#[derive(Default)]
+pub struct ConnectionParams {
+    pub account_name: String,
+    pub username: String,
+    pub password: String,
+    pub warehouse: String,
+    pub role: String,
+    pub database: String,
+    pub schema: String,
+}
+
+pub struct SnowflakeClient {
+    host: String,
+    session: Session,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Session {
+    token: String,
+    session_id: u64,
+}
+
+/// Based on https://github.com/joshuataylor/snowflake_elixir/blob/master/lib/snowflake_elixir/http/snowflake_client.ex
+impl SnowflakeClient {
+    pub async fn login(params: ConnectionParams) -> reqwest::Result<SnowflakeClient> {
+        let host = format!("https://{}.snowflakecomputing.com", params.account_name);
+
+        let url = Url::parse_with_params(
+            &format!("{host}/session/v1/login-request"),
+            [
+                ("warehouse", &params.warehouse),
+                ("roleName", &params.role),
+                ("databaseName", &params.database),
+                ("schemaName", &params.schema),
+            ],
+        )
+        .expect("URL is wrong");
+
+        let body = json!({
+            "data": {
+                "ACCOUNT_NAME": params.account_name,
+                "PASSWORD": params.password,
+                "CLIENT_APP_ID": "JavaScript",
+                "CLIENT_APP_VERSION": "1.5.3",
+                "LOGIN_NAME": params.username,
+                "SESSION_PARAMETERS": {
+                    "VALIDATE_DEFAULT_PARAMETERS": true,
+                    "QUOTED_IDENTIFIERS_IGNORE_CASE": true,
+                },
+                "CLIENT_ENVIRONMENT": {
+                    "APPLICATION": "SnowflakeEx",
+                    "OCSP_MODE": "FAIL_OPEN",
+                    "OS": "Linux",
+                    "tracing": "DEBUG",
+                    "account": params.account_name,
+                    "user": params.username,
+                    "warehouse": params.warehouse,
+                    "database": params.database,
+                    "schema": params.schema,
+                }
+            }
+        });
+
+        let response = Client::new()
+            .post(url)
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .json(&body)
+            .timeout(Duration::from_secs(120))
+            .send()
+            .await?;
+
+        #[derive(Deserialize)]
+        struct LoginResponse {
+            data: Session,
+        }
+
+        let result: LoginResponse = response.json().await?;
+
+        Ok(SnowflakeClient {
+            host,
+            session: result.data,
+        })
+    }
+
+    pub async fn query(&self, query: &str) -> reqwest::Result<()> {
+        let res = Client::new()
+            .post(format!(
+                "{}/queries/v1/query-request?requestId={}",
+                self.host,
+                Uuid::new_v4()
+            ))
+            .header("Content-Type", "application/json")
+            .header("accept", "application/snowflake")
+            .header(
+                "Authorization",
+                format!("Snowflake Token=\"{}\"", self.session.token),
+            )
+            .json(&json!({
+                "sqlText": query,
+                "sequenceId": 0,
+                "bindings": null,
+                "bindStage": null,
+                "describeOnly": false,
+                "parameters": { "CLIENT_RESULT_CHUNK_SIZE": 48 },
+                "describedJobId": null,
+                "isInternal": false,
+                "asyncExec": false,
+            }))
+            .send()
+            .await?;
+
+        dbg!(res.status());
+        println!("{}", res.text().await?);
+
+        // TODO: Manage results
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +132,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn it_works() {}
 }
